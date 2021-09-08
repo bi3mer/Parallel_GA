9a3526aca7ea9878caabed6433b07dea9046ae26
bi3mer <colan@comcast.net>
2021-09-08 15:52:32 -0400

    fixed buf with fitness TSP calculation and I'm going to add simulated annealing

--- Problems/TSP.py
+++ Problems/TSP.py
@@ -6,6 +6,7 @@ from random import shuffle
 
 cities = TSPLib.load(join(expanduser('~'), 'data', 'TSPLib', 'pr76.tsp'))
 
+
 strand_size = len(list(cities.get_nodes()))
 
 max_distance = 1000000
@@ -22,7 +23,7 @@ mutation_rate = 0.02
 
 network_run_percentage = 0.9
 
-run_time = 20
+run_time = 45
 
 def create_strand():
     strand = [i + 1 for i in range(strand_size)]
@@ -33,7 +34,7 @@ def fitness(strand):
     assert len(strand) == strand_size
 
     result = 0
-    for current in range(0, len(strand)):
+    for current in range(1, len(strand)):
         result += cities.get_weight(strand[current-1], strand[current])
 
     result += cities.get_weight(strand[-1], strand[0])
@@ -59,6 +60,11 @@ def crossover(p_1, p_2):
 
     return p_1, p_2
 
+test = [0, 22, 21, 20, 24, 23, 45, 44, 43, 47, 46, 68, 67, 69, 66, 49, 48, 52, 26, 25, 19, 18, 30, 29, 28, 31, 32, 27, 42, 41, 53, 51, 50, 65, 64, 55, 54, 57, 56, 70, 71, 72, 63, 62, 61, 60, 58, 59, 40, 39, 33, 34, 38, 37, 35, 36, 17, 16, 15, 14, 73, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 74, 75]
+test = [i + 1 for i in test]
+print(fitness(test))
+import sys
+sys.exit(-1)
 
 '''
 Useful Stuff
--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from Optimization import *
 from Problems import *
 from Networks import *
 
+from time import time
+
 seed = 1
 
-print('GA')
 ga = GA(TSP, rng_seed=seed)
-for res in ga.run()[:5]:
+start = time()
+ga_solutions = ga.run()
+end = time()
+print(f'GA ({end - start})')
+for res in ga_solutions[:5]:
     print(res[0])
 
-print('Ring Lattice')
 ring_lattice = IslandGA(TSP, ring_lattice)
-for res in ring_lattice.run()[:5]:
+start = time()
+rl_solutions = ring_lattice.run()
+end = time()
+
+print()
+print(f'Ring Lattice ({end - start})')
+for res in rl_solutions[:5]:
     print(res[0])
