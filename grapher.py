110c2a0c9fcad61b2901a29b2534cbd54769ef2f
bi3mer <colan@comcast.net>
2021-09-29 14:37:02 -0400

    analysis

--- grapher.py
+++ grapher.py
@@ -3,17 +3,17 @@ from Utility import Database as db
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
-## Epochs till migration graph
-df = pd.DataFrame(columns=['Algorithm', 'Fitness Calculations', 'Fitness'])
+# Epochs till migration graph
+df = pd.DataFrame(columns=['Algorithm', 'Epoch', 'Migration Rate', 'Fitness'])
 
 configs = db.con.execute('''
     select * 
     from config 
     where 
         name="TSP" and 
-        population_size=320 and
-        epochs_till_migration = 2
+        population_size=320 
     order by 
         fitness_calculations
 ''').fetchall()
@@ -35,12 +35,27 @@ for unnamed_tuple in configs:
         
         df = df.append({
             'Algorithm': c.algorithm,
-            'Fitness Calculations': c.fitness_calculations,
+            'Epoch': c.epochs_till_migration,
+            'Migration Rate': c.migration_rate,
             'Fitness': r.fitness
         }, ignore_index=True)
 
 
-sns.set_theme(style="darkgrid")
+df_full = df[df['Algorithm'] == 'Island GA Ring Lattice']
 
-sns.lineplot(data=df, x='Fitness Calculations', y='Fitness', hue='Algorithm')
+X, Y = np.meshgrid(df_full['Epoch'], df_full['Migration Rate'])
+# X = [1,2,3,4,5]
+# Y = [1,3,5,7,9]
+# Z = [1,4,9,25,36]
+# X,Y = np.meshgrid(X, Y)
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+# surf = ax.plot_wireframe(X, Y, np.array([[x] for x in df_full['Fitness']]))
+# surf = ax.plot_wireframe(X, Y, np.array([[x] for x in df_full['Fitness']]))
+# ax.scatter(df_full['Epoch'], df_full['Migration Rate'], np.array([[x] for x in df_full['Fitness']]))
+ax.plot_trisurf(df_full['Epoch'], df_full['Migration Rate'], df_full['Fitness'])
 plt.show()
+
+# sns.set_theme(style="darkgrid")
+
+# sns.lineplot(data=df, x='Fitness Calculations', y='Fitness', hue='Algorithm')
+# plt.show()
